71bd6149758fb0d6945358aec4e36dc79ab4d7d3
Alexander077 <renzy.max@gmail.com>
2015-03-17 07:11:15 -0600

    Minor code reorganization

---
+++ AutoText/AutotextExpression.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AutoText
+{
+	public class AutotextExpression
+	{
+		public string ExpressionText { get; set; }
+		public int StartIndex { get; set; }
+		public int Length { get; set; }
+		public List<AutotextExpression> NestedExpressions { get; set; }
+
+		public AutotextExpression()
+		{
+			NestedExpressions = new List<AutotextExpression>(100);
+		}
+
+		public AutotextExpression(string expressionText, int startIndex, int length)
+		{
+			ExpressionText = expressionText;
+			StartIndex = startIndex;
+			Length = length;
+			NestedExpressions = new List<AutotextExpression>(100);
+		}
+
+		private List<Input> GenerateExpressionInput()
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<Input> GetInput()
+		{
+
+			throw new NotImplementedException();
+		}
+	}
+}
